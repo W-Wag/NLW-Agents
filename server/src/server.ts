82ad3e77b704ed81d5b63b807fe0418497c07366
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2025-07-09 14:17:10 -0300

    Feat: criação de questões, criação de sala e get de questões da sala

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,9 @@ import {
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { env } from "./env.ts";
+import { createQuestion } from "./http/routes/create-question.ts";
+import { createRoomRoute } from "./http/routes/create-room.ts";
+import { getRoomQuestionsRoute } from "./http/routes/get-room-questions.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -22,4 +25,7 @@ app.get("/health", () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(createQuestion);
+app.register(getRoomQuestionsRoute);
 app.listen({ port: env.PORT });
